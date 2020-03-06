e03c999bb78e3d3f58461d262b530421f8e70d27
Dhoms Pelago <dhoms1102@gmail.com>
2020-03-07 00:22:23 +0800

    add other data in git ignore fix error

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/coreproc/MyRepo/npl/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/coreproc/MyRepo/npl/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\natural-language-processing\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\natural-language-processing\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
